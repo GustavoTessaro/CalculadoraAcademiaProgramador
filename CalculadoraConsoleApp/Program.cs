717534f8cb17388ba7b320f99511572bd8b9c4ff
Gustavo Tessaro <gu.tessaro@hotmail.com>
2026-03-05 15:31:03 -0300

    adicionando tabuada

--- CalculadoraConsoleApp/Program.cs
+++ CalculadoraConsoleApp/Program.cs
@@ -10,6 +10,78 @@ double num1 = 0, num2 = 0;
 while (continuar == true)
 {
     Boolean confirmacao = true;
+    Boolean confirmacao2 = true;
+    Boolean voltarTabuada = false;
+
+    Console.WriteLine("Escolha a operação: +, -, *, /, Tabuada");
+    string operacao = Console.ReadLine();
+
+    if (String.Equals(operacao, "Tabuada", StringComparison.OrdinalIgnoreCase))
+    {
+
+        double numeroTabuada = 0;
+
+        try
+        {
+            Console.WriteLine("Digite o número para a tabuada:");
+            numeroTabuada = Convert.ToDouble(Console.ReadLine());
+
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Erro: Por favor, digite um número válido.");
+            continue;
+        }
+
+        Console.WriteLine($"Tabuada do {numeroTabuada}:");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{numeroTabuada} x {i} = {numeroTabuada * i}");
+        }
+
+        while (confirmacao2 == true)
+        {
+            Console.WriteLine("Deseja realizar outra operação? (s/n)");
+            string resposta = Console.ReadLine().ToLower();
+
+            if (String.Equals(resposta, "n", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Obrigado por usar a calculadora. Até mais!");
+                continuar = false;
+                break;
+            }
+            else
+            {
+                if (String.Equals(resposta, "s", StringComparison.OrdinalIgnoreCase))
+                {
+                    confirmacao2 = false;
+                    voltarTabuada = true;
+                }
+                else
+                {
+                    Console.WriteLine("Resposta inválida. Por favor, digite 's' para sim ou 'n' para não.");
+                }
+            }
+
+        }
+
+    }
+    else
+        if (operacao != "+" && operacao != "-" && operacao != "*" && operacao != "/")
+        {
+            Console.WriteLine("Operação inválida. Tente novamente.");
+            continue;
+        }
+
+    if (voltarTabuada == true)
+    {
+        continue;
+    }
+
+    if (continuar == false)
+    {
+        break;
+    }
 
     while (confirmacao == true)
     {
@@ -30,8 +102,7 @@ while (continuar == true)
         }
     }
 
-    Console.WriteLine("Escolha a operação: +, -, *, /");
-    string operacao = Console.ReadLine();
+
 
     double resultado = 0;
 
@@ -63,8 +134,6 @@ while (continuar == true)
     }
 
     Console.WriteLine($"Resultado: {resultado}");
-
-    Boolean confirmacao2 = true;
 
     while (confirmacao2 == true)
     {
